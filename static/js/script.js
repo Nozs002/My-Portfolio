96dde82ce28505660ed8467a88f2199bd5a44df4
Nozs002 <phamson26082005@gmail.com>
2026-02-03 16:45:31 +0700

    Updates on personal data security

--- static/js/script.js
+++ static/js/script.js
@@ -39,3 +39,11 @@ const spline = document.querySelector("spline-viewer");
 spline.addEventListener("load", (e) => {
   console.log("Scene 3D đã tải xong!");
 });
+
+const links = document.querySelectorAll(".dark-list a");
+
+// Lặp qua từng link và thêm thuộc tính
+links.forEach((link) => {
+  link.setAttribute("target", "_blank");
+  link.setAttribute("rel", "noopener noreferrer");
+});
